ff157514a4ea5698ccb925751ba20fda29ed5033
Candace Van Doren <cvd2586@gmail.com>
2019-10-19 15:23:24 +0000

    Add previewing email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "cvd2586@gmail.com",
+    mail(to: "candacemvandoren@gmail.com",
       subject: "A comment has been added to your place.")
   end
 end
